ba0e94749b58aa76a5fcb95f069516859a6fcbbd
jchua1 <jchua@stuy.edu>
2015-11-17 21:40:10 -0500

    Added comments

--- Rational.java
+++ Rational.java
@@ -1,12 +1,23 @@
+/*
+  Team W-Fd -- Jason Chua, Ricky Wu
+  APCS1 pd9
+  HW32 -- Irrationality Stops Here
+  2015-11-17
+*/
+
 public class Rational {
 
+    //instance variables for numerator and denominator
     private int num, denom;
 
+    //default constructor that sets numerator to 0 and denominator to 1
     public Rational() {
 	num = 0;
 	denom = 1;
     }
 
+    //overloaded constructor that takes two arguments to set numerator and denominator
+    //if given denominator is 0, numerator is set to 0 and denominator is set to 1 instead
     public Rational(int x, int y) {
 	this();
 	if (y == 0) {
@@ -18,21 +29,25 @@ public class Rational {
 	}
     }
 
+    //returns numerator and denominator separated by a /
     public String toString() {
 	String retStr = "";
 	retStr += num + "/" + denom;
 	return retStr;
     }
 
+    //returns floating point value of the number by changing numerator to double and then dividing by the denominator
     public double floatValue() {
         return ((double)num / denom);
     }
 
+    //multiplies the numerator and denominator by those of the given Rational
     public void multiply(Rational x) {
         num *= x.num;
 	denom *= x.denom;
     }
 
+    //divides the numerator and denominator by those of the given Rational
     public void divide(Rational x) {
         num *= x.denom;
 	denom *= x.num;
